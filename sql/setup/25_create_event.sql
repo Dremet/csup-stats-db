7a51ba10857528fcf38c29b4ac3090e1a05b9aac
Andre Petersen <mail@andrepetersen.de>
2022-01-16 20:54:32 +0100

    Changed datetime to date

--- sql/setup/25_create_event.sql
+++ sql/setup/25_create_event.sql
@@ -3,7 +3,7 @@ drop table if exists base.events;
 create table base.events (
     e_id serial primary key,
     s_s_id int not null,
-    e_datetime timestamp not null,
+    e_date date not null,
     e_uses_event_points boolean not null,
     e_points_pos_1 smallint not null,
     e_points_pos_2 smallint not null,
@@ -19,5 +19,5 @@ create table base.events (
     e_points_pos_12 smallint not null,
     e_points_for_pole smallint not null,
     e_points_for_fastest_lap smallint not null,
-    UNIQUE (s_s_id, e_datetime)
+    UNIQUE (s_s_id, e_date)
 );
