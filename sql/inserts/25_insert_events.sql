252e2f55c1f8d90229a4d48ab8dd9180f6c1b95a
Andre Petersen <mail@andrepetersen.de>
2022-01-19 19:02:50 +0100

    Added functionality to insert events

--- sql/inserts/25_insert_events.sql
+++ sql/inserts/25_insert_events.sql
@@ -20,22 +20,22 @@ insert into base.events(
 values
 (
     %(s_s_id)s,
-    %(e_date)s,
-    %(e_uses_event_points)s,
-    %(e_points_pos_1)s,
-    %(e_points_pos_2)s,
-    %(e_points_pos_3)s,
-    %(e_points_pos_4)s,
-    %(e_points_pos_5)s,
-    %(e_points_pos_6)s,
-    %(e_points_pos_7)s,
-    %(e_points_pos_8)s,
-    %(e_points_pos_9)s,
-    %(e_points_pos_10)s,
-    %(e_points_pos_11)s,
-    %(e_points_pos_12)s,
-    %(e_points_for_pole)s,
-    %(e_points_for_fastest_lap)s
+    %(date)s,
+    %(uses_event_points)s,
+    %(points_pos_1)s,
+    %(points_pos_2)s,
+    %(points_pos_3)s,
+    %(points_pos_4)s,
+    %(points_pos_5)s,
+    %(points_pos_6)s,
+    %(points_pos_7)s,
+    %(points_pos_8)s,
+    %(points_pos_9)s,
+    %(points_pos_10)s,
+    %(points_pos_11)s,
+    %(points_pos_12)s,
+    %(points_for_pole)s,
+    %(points_for_fastest_lap)s
 )
 on conflict (s_s_id, e_date)
 do 
---
+++ upsert_events.py
@@ -0,0 +1,56 @@
+import argparse
+import pandas as pd
+from pathlib import Path
+from connection import Cursor, Connection
+from helpers import read_sql_insert_template, ROOT_PATH
+
+### USAGE ###
+# python3 upsert_events.py -c ICST -l Superstars -s 2
+parser = argparse.ArgumentParser(description='Upsert events for a specific championship, league and season')
+parser.add_argument('-c', '--championship', help='Championship', type=str, required=True)
+parser.add_argument('-l', '--league', help='League', type=str, required=True)
+parser.add_argument('-s', '--season', help='Season', type=str, required=True)
+
+args = parser.parse_args()
+
+championship, league, season = args.championship, args.league, args.season
+
+# check if folder exists
+season_path = ROOT_PATH / Path(championship, league) / Path("Season"+season)
+assert season_path.is_dir(), f"This folder does not exist: {season_path}"
+
+# now check if events.csv file exists
+events_file_path = season_path / Path("events.csv")
+
+events = pd.read_csv(events_file_path)
+events["date"] = events["date"].astype(str).apply(lambda x: pd.Timestamp(x))
+
+print(events)
+
+with Connection() as conn:
+    season = pd.read_sql("select s_id "
+        "from base.seasons s "
+        f"left join base.leagues l on l_name = '{league}' "
+        f"left join base.championships c on c_name = '{championship}' "
+        f"where s.s_desc = '{season}' and s.l_l_id = l.l_id and l.c_c_id = c.c_id", 
+    con=conn)
+
+    print("season", season)
+
+    assert len(season) == 1, "Found no or more that one s_id, exciting.."
+
+    sid = season["s_id"].values[0]
+
+    print(sid)
+
+    events["s_s_id"] = sid
+
+    sql = read_sql_insert_template("25_insert_events.sql")
+
+    for _, row in events.iterrows():
+        input_data = list(row)
+        
+        print(f"Upserting data for this event: championship {championship}, league {league}, season {season}, event date {input_data[1]}")
+        print(row)
+
+        conn.cursor().execute(sql, dict(zip(events.columns, input_data)))
