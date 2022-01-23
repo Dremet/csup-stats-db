adb0b2f0542bcd6976a953193dfb385848f91ba2
Andre Petersen <mail@andrepetersen.de>
2022-01-23 07:35:30 +0100

    Truncate all sql

---
+++ sql/cleanup/truncate_all.sql
@@ -0,0 +1,13 @@
+truncate base.championships;
+truncate base.leagues;
+truncate base.seasons;
+truncate base.events;
+truncate base.event_points;
+truncate base.races;
+truncate base.quali_results;
+truncate base.race_results;
+truncate base.lap_results;
+truncate base.drivers;
+--truncate base.driver_numbers;
+truncate base.teams;
+truncate base.team_mappings;
